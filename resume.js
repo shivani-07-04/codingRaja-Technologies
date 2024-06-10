50d0f5767c1480166fac9099f3fa7bd2f54d02af
shivani-07-04 <160331623+shivani-07-04@users.noreply.github.com>
2024-06-10 21:36:02 +0530

    resume.js

---
+++ resume.js
@@ -0,0 +1,4 @@
+function hide() {
+    // Add your JavaScript code here to generate and edit the resume
+    // You can use the form data to create a resume template and display it in the output container
+}
